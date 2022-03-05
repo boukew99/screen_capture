9c465a2138c575df01d9aa38dda9982ed055afc0
boukew99 <boukewestra0@gmail.com>
2022-03-05 22:42:18 +0100

    set capture rect once + popup

--- addons/screen_capture/screen_capture.gd
+++ addons/screen_capture/screen_capture.gd
@@ -1,11 +1,6 @@
 extends FileDialog
 
-var capture_rect = Rect2(Vector2.ZERO, OS.window_size)
- 
-# useful with $"NodePath".get_global_rect()
-func capture(global_rect):
-	capture_rect = global_rect
-	popup_centered()
+var capture_rect = Rect2(Vector2.ZERO, OS.window_size) # useful with $"NodePath".get_global_rect()
 	
 func _on_ScreenCapture_file_selected(path):
 	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
